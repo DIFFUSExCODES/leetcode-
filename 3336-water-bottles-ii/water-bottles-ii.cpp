b96e22957d0f511cd34588fe05500b480ef53265
SHIVANSH PANWAR <shivanshpanwar.codes@gmail.com>
2025-10-02 19:22:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (77.56%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.cpp
+++ 3336-water-bottles-ii/water-bottles-ii.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int maxBottlesDrunk(int numBottles, int numExchange) {
         int emptyBottles = 0 , BottlesDrunk = 0 ;
+        emptyBottles += numBottles ;
+        BottlesDrunk += numBottles ;
+        numBottles = 0 ;
         while(true){
-            emptyBottles += numBottles ;
-            BottlesDrunk += numBottles ;
-            numBottles = 0 ;
             if(emptyBottles >= numExchange){
                 emptyBottles -= numExchange ;
-                numBottles++ ;
+                BottlesDrunk++ ;
+                emptyBottles++ ;
                 numExchange++ ;
             }
-            if(emptyBottles < numExchange && numBottles == 0) return BottlesDrunk ;
+            else return BottlesDrunk ;
         }
     }
 };
