76f9015580538cbeb1d38922ac8ab5fec93f2132
SHIVANSH PANWAR <shivanshpanwar.codes@gmail.com>
2025-10-25 14:21:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.74%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -5,10 +5,8 @@ public:
         int money = 0 ;
         for(int i = 0 ; i < w ; i++ ){
             money += (7+1+i)*(7+i)/2 - (1+i)*(i)/2 ;
-            cout<< money << "\n";
         }
         money += (n%7 + w )*(n%7 + w + 1)/2 - (w+1)*(w)/2 ;
-        cout<< money;
         return money ;
     }
 };
